513f0cbd707d4399e50e566e616f060446b7d06b
RafayetHossen <hossenmdrafayet@gmail.com>
2025-01-18 23:43:50 +0600

    Create header component and style them

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
+import Header from './components/Header';
 import './styles.css'
 const App = () => {
   return (
     <div>
-      <header className="mt-12">
-        <h1>Welcome to MovieDux</h1>
-      </header>
-      <footer className="mt-24 bg-[#2c2c2c] p-4 text-center">
-        <h1>This is footer content</h1>
-      </footer>
+      <div className='container'>
+        <Header />
+      </div>
+      
+      
     </div>
   );
 };

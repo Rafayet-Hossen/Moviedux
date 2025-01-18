3154bda91a28fa64dc8a1c8e0492b5847125d6be
RafayetHossen <hossenmdrafayet@gmail.com>
2025-01-18 23:27:07 +0600

    Setup the movie dux website in react with vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import './styles.css'
 const App = () => {
   return (
     <div>
-        
+      <header className="mt-12">
+        <h1>Welcome to MovieDux</h1>
+      </header>
+      <footer className="mt-24 bg-[#2c2c2c] p-4 text-center">
+        <h1>This is footer content</h1>
+      </footer>
     </div>
   );
 };

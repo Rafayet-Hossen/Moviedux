2b8674a776b5e0f3f9ad8c565d6309f5fd09879f
RafayetHossen <hossenmdrafayet@gmail.com>
2025-01-20 00:14:36 +0600

    Error Handling if the image not found

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -1,17 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const MovieCard = ({movie}) => {
-    const {id,image,title,genre,rating} = movie;
-    return (
-      <div key={id} className="movie-card">
-        <img src={`images/${image}`} alt="" />
-        <div className="movie-card-info">
-          <h3 className="movie-card-title">{title}</h3>
-          <p className="movie-card-genre">{genre}</p>
-          <p className="movie-card-rating">{rating}</p>
-        </div>
+const MovieCard = ({ movie }) => {
+  const { id, image, title, genre, rating } = movie;
+
+  const handleError = (event) => {
+    event.target.src = "images/default.jpg";
+  };
+
+  return (
+    <div key={id} className="movie-card">
+      <img src={`images/${image}`} alt="" onError={handleError} />
+      <div className="movie-card-info">
+        <h3 className="movie-card-title">{title}</h3>
+        <p className="movie-card-genre">{genre}</p>
+        <p className="movie-card-rating">{rating}</p>
       </div>
-    );
+    </div>
+  );
 };
 
 export default MovieCard;

704b69ab6bfcec9f04ade15b498f7f4d79b482e0
RafayetHossen <hossenmdrafayet@gmail.com>
2025-01-20 00:26:05 +0600

    Set color based on the movie rating

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -7,13 +7,24 @@ const MovieCard = ({ movie }) => {
     event.target.src = "images/default.jpg";
   };
 
+  const getRating = (rating) => {
+    if(rating >= 8){
+        return 'rating-good'
+    }
+    else if(rating<8 && rating>=5){
+        return 'rating-ok'
+    }
+    else{
+        return 'rating-bad'
+    }
+  }
   return (
     <div key={id} className="movie-card">
       <img src={`images/${image}`} alt="" onError={handleError} />
       <div className="movie-card-info">
         <h3 className="movie-card-title">{title}</h3>
         <p className="movie-card-genre">{genre}</p>
-        <p className="movie-card-rating">{rating}</p>
+        <p className={`movie-card-rating ${getRating(rating)}`}>{rating}</p>
       </div>
     </div>
   );

6593dfd87f38f5ca2bd69edba78fe2d66f21b698
RafayetHossen <hossenmdrafayet@gmail.com>
2025-01-19 22:43:37 +0600

    Create MovieGrid component and style them

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Footer from './components/Footer';
 import Header from './components/Header';
+import MoviesGrid from './components/MoviesGrid';
 import './styles.css'
 const App = () => {
   return (
     <div>
       <div className='container'>
         <Header />
+        <MoviesGrid/>
       </div>
       
       <Footer/>

2ca019a1167637fa64dd799ab7e2dc57291d91ee
RafayetHossen <hossenmdrafayet@gmail.com>
2025-01-20 00:48:56 +0600

    Implement dynamic search in moviedux

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -1,23 +1,41 @@
-import React, { useEffect, useState } from 'react';
-import '../styles.css'
-import MovieCard from './MovieCard';
+import React, { useEffect, useState } from "react";
+import "../styles.css";
+import MovieCard from "./MovieCard";
 const MoviesGrid = () => {
+  const [movies, setMovies] = useState([]);
 
-    const [movies,setMovies] = useState([]);
-    useEffect(() => {
-       fetch('movies.json')
-       .then(response => response.json())
-       .then(data => setMovies(data))
-    },[]);
-    return (
-        <div className='movies-grid'>
-            {
-                movies.map((movie) => (
-                    <MovieCard movie={movie} key={movie.id}/>
-                ))
-            }
-        </div>
-    );
+  const [searchTerm, setSearchTerm] = useState("");
+
+  const handleSearchChange = (event) => {
+    setSearchTerm(event.targer.value);
+  };
+
+  const filteredMovies = movies.filter((movie) =>
+    movie.title.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  useEffect(() => {
+    fetch("movies.json")
+      .then((response) => response.json())
+      .then((data) => setMovies(data));
+  }, []);
+
+  return (
+    <div>
+      <input
+        type="text"
+        className="search-input text-gray-600"
+        placeholder="Search movies..."
+        value={searchTerm}
+        onChange={handleSearchChange}
+      />
+      <div className="movies-grid">
+        {filteredMovies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MoviesGrid;
